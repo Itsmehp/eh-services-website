1059f5bd2339df14841a7a64c12b81fd0ac85013
Itsmehp <harshil.hpz@gmail.com>
2026-01-23 13:43:50 +0100

    chore: add minimal flat eslint.config.cjs as temporary workaround

---
+++ eslint.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  ignores: [
+    "node_modules/**",
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ],
+  rules: {
+    // deliberately minimal temporary config to avoid circular config validation errors
+  },
+};
